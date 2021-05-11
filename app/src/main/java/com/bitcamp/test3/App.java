2a6c68b69c224f6ad963b0429d6103b7127e8d08
leeheejin0 <jinsis0908@naver.com>
2021-05-11 11:17:59 +0900

    .

--- app/src/main/java/com/bitcamp/test3/App.java
+++ app/src/main/java/com/bitcamp/test3/App.java
@@ -12,6 +12,7 @@ public class App {
     System.out.println(new App().getGreeting());
     System.out.println("이게 머선129");
     System.out.println("abcdef");
+    System.out.println("아아아아아아아아");
     System.out.println("ghijklmnop");
     System.out.println("qrstuvwxyz");
     System.out.println("푸쉬 한번하기 너무 힘들어요. 계속 충돌나요ㅕ");
